925e75b848ae341ca7477c0f6a16a23d5dba0e13
Coderrrrs <87891833+Coderrrrs@users.noreply.github.com>
2022-07-10 12:03:20 +0530

    Delete input_time.c

--- input_time.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main(){
-    int hour,minute;
-    printf("Enter the time in given format:(HH:MM) : ");
-    scanf("%d:%d",&hour,&minute);
-    printf("%02d hour and %02d minute",hour,minute);
-}
