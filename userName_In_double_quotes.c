a9f3e7720e93ebf9320c6d3dd99870657d39320c
Coderrrrs <87891833+Coderrrrs@users.noreply.github.com>
2022-07-10 12:03:59 +0530

    Delete userName_In_double_quotes.c

--- userName_In_double_quotes.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main(){
-    char name[1000];
-    printf("Enter your name : ");
-    scanf("%[^\n]s",name);
-    printf("\"Hello , %s\"",name);
-    getch();
-}
